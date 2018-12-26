94f3895b43add81de210cbfce88893eedda2a22d
lvliukun <791419612@qq.com>
2018-12-26 09:36:38 +0800

    91游戏model

--- Baocms/Lib/Action/App/JiuyiAction.class.php
+++ Baocms/Lib/Action/App/JiuyiAction.class.php
@@ -134,7 +134,10 @@ class JiuyiAction extends CommonAction
         //创建库存-3
         $data['goods_name']='测试商品';$data['goods_header']='测试商品测试商品测试商品测试商品测试商品';$data['goods_img']='图片';$data['strike_price']='15000';$data['auction_price']='5000';
         $data['buyback_price']='2000';$data['auction_num']='10';$data['inventory_num']='100';$data['sold_out']='1';$data['creatime']=time();
-        D('Jiuyi')->creategoods($data);
+        //D('Jiuyi')->creategoods($data);
+        print_r(unserialize(Cac()->get('jiuyi_auction_43')));
+        print_r(Cac()->get('jiuyi_periods_num_43'));
+        print_r(Cac()->lrange('jiuyi_auction_list_74',0,-1));
     }
     /**竞拍成功商品期数展示详细
      * @param goods_id 商品id
